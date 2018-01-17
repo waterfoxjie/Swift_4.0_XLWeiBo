5cf12367960248bc7f9d28d9df5696084887d3eb
waterfoxjie <waterfoxjie@163.com>
2018-01-17 16:27:45 +0800

    1、设置启动控制器 2、添加 Bundle extension 3、添加 TabBar 图片

--- XLWeiBo/XLWeiBo/AppDelegate.swift
+++ XLWeiBo/XLWeiBo/AppDelegate.swift
@@ -16,7 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // 设置启动
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = UIColor.white
+        window?.rootViewController = XLMainTabBarController()
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
---
+++ XLWeiBo/XLWeiBo/Classes/Tools/Extension/Bundle+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  Bundle+Extension.swift
+//  XLWeiBo
+//
+//  Created by waterfoxjie on 2018/1/17.
+//  Copyright © 2018年 waterfoxjie. All rights reserved.
+//
+
+import Foundation
+
+extension Bundle {
+    // 拿到 BundleName 的值，并设置成一个计算型属性进行返回
+    var nameSpace: String {
+        return infoDictionary?["CFBundleName"] as? String ?? ""
+    }
+}
