5ec90c998afe9b0c7337e61f4b2c8ae5dcb5b723
waterfoxjie <waterfoxjie@163.com>
2018-01-18 11:50:18 +0800

    BaseViewController 中添加 NavBar 按钮方法（之后的项目也可以使用）

--- XLWeiBo/XLWeiBo/Classes/View/Main/XLBaseViewController.swift
+++ XLWeiBo/XLWeiBo/Classes/View/Main/XLBaseViewController.swift
@@ -12,24 +12,91 @@ class XLBaseViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        setupUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func setupUI() {
+        view.backgroundColor = UIColor.white
     }
-    */
-
+    
+    // MARK: - 设置添加按钮方法
+    /// 添加左边文字按钮
+    ///
+    /// - Parameters:
+    ///   - title: title
+    ///   - target: target
+    ///   - action: action
+    func addLeftTitleBarButton(title: String,
+                               target: Any?,
+                               action: Selector) {
+        navigationItem.leftBarButtonItem = setupTitleBarButton(title: title,
+                                                               target: target,
+                                                               action: action)
+    }
+    
+    /// 添加左边文字返回按钮
+    ///
+    /// - Parameter title: title
+    func addBackTitleBarButton(title: String) {
+        navigationItem.leftBarButtonItem = setupTitleBarButton(title: title,
+                                                               target: self,
+                                                               action: #selector(clickBackButton))
+    }
+    
+    /// 添加左边图片返回按钮
+    ///
+    /// - Parameters:
+    ///   - normalImageName: normalImageName
+    ///   - highlightedImageName: highlightedImageName
+    func addBackImageBarButton(normalImageName: String,
+                               highlightedImageName: String) {
+        let imageButton = UIBarButtonItem(normalImageName: normalImageName,
+                                          highlightedImageName: highlightedImageName,
+                                          target: self,
+                                          action: #selector(clickBackButton))
+        navigationItem.leftBarButtonItem = imageButton
+    }
+    
+    /// 添加返回按钮（默认图片 + 返回文字）
+    func addBackButton() {
+        let barButton = UIBarButtonItem(title: "返回",
+                                        normalImageName: "NavReturnIcon",
+                                        highlightedImageName: "NavReturnIcon",
+                                        target: self,
+                                        action: #selector(clickBackButton))
+        navigationItem.leftBarButtonItem = barButton
+    }
+    
+    /// 添加右边文字按钮
+    ///
+    /// - Parameters:
+    ///   - title: title
+    ///   - target: target
+    ///   - action: action
+    func addRightTitleBarButton(title: String,
+                                target: Any?,
+                                action: Selector) {
+        navigationItem.rightBarButtonItem = setupTitleBarButton(title: title,
+                                                                target: target,
+                                                                action: action)
+    }
+    
+    // MARK: - 创建按钮方法
+    private func setupTitleBarButton(title: String,
+                                     target: Any?,
+                                     action: Selector) -> UIBarButtonItem {
+        let barButtonItem = UIBarButtonItem(title: title,
+                                            target: target,
+                                            action: action)
+        return barButtonItem
+    }
+    
+    // MARK: - 返回按钮事件
+    @objc private func clickBackButton() {
+        if navigationController?.childViewControllers.count == 1 {
+            navigationController?.dismiss(animated: true, completion: nil)
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
+    }
 }
