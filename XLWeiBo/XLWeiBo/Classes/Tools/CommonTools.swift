acf5816a0c9d54c45c7a54d0db4bb250bebd410c
waterfoxjie <waterfoxjie@163.com>
2018-01-24 14:03:24 +0800

    添加应用程序信息

--- XLWeiBo/XLWeiBo/Classes/Tools/CommonTools.swift
+++ XLWeiBo/XLWeiBo/Classes/Tools/CommonTools.swift
@@ -8,14 +8,23 @@
 
 import UIKit
 
+// MARK: - 应用程序信息
+// 应用程序 ID
+let WeiBoAppKey = "2969513224"
+// 应用程序加密信息（开发者可以申请修改）
+let WeiBoAppSecret = "877b3fb57f9dc49b6a5d92bc75631a69"
+// 应用程序回调地址
+let WeiBoRedirectUri = "http://www.baidu.com"
+
 // 系统信息
 let ScreenHeight = UIScreen.main.bounds.size.height
 let ScreenWidth = UIScreen.main.bounds.size.width
 
-// 登录通知
-let WeiBoUserLoginNotification = "WeiBoUserLoginNotification"
-
 // 按钮文字颜色
 let NormalTitleColor: UIColor = UIColor.darkGray
 let HighlightedTitleColor: UIColor = UIColor.orange
+
+// MARK: - 全局通知定义
+// 登录通知
+let WeiBoUserLoginNotification = "WeiBoUserLoginNotification"
 
