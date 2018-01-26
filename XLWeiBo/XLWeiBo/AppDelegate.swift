4a8c359c44e13d999138359ece997ee68a30e003
waterfoxjie <waterfoxjie@163.com>
2018-01-26 17:39:33 +0800

    设置 SVProgressHUD 最小解除时间以及网络指示器

--- XLWeiBo/XLWeiBo/AppDelegate.swift
+++ XLWeiBo/XLWeiBo/AppDelegate.swift
@@ -9,6 +9,8 @@
 import UIKit
 import CoreData
 import UserNotifications
+import SVProgressHUD
+import AFNetworking
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,15 +20,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        if #available(iOS 10.0, *) {
-            UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .carPlay, .sound]) { (success, error) in
-                print("授权 " + (success ? "成功" : "失败"))
-            }
-        } else {
-            // 获取用户授权通知（上方的提示条、声音、数字）
-            let notiSettings = UIUserNotificationSettings(types: [.alert, .badge, .alert], categories: nil)
-            application.registerUserNotificationSettings(notiSettings)
-        }
+        // 设置应用程序额外信息
+        setupAppAdditions()
         
         // 设置启动
         window = UIWindow(frame: UIScreen.main.bounds)
@@ -37,24 +32,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
@@ -62,7 +39,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     // MARK: - Core Data stack
-
     lazy var persistentContainer: NSPersistentContainer = {
         /*
          The persistent container for the application. This implementation
@@ -73,17 +49,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let container = NSPersistentContainer(name: "XLWeiBo")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
@@ -91,7 +56,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }()
 
     // MARK: - Core Data Saving support
-
     func saveContext () {
         let context = persistentContainer.viewContext
         if context.hasChanges {
@@ -105,6 +69,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
     }
+}
 
+// MARK: - 设置应用程序额外信息
+extension AppDelegate {
+    private func setupAppAdditions() {
+        // 设置 SVProgressHUD 最小解除时间
+        SVProgressHUD.setMinimumDismissTimeInterval(1)
+        // 设置网络加载指示器
+        AFNetworkActivityIndicatorManager.shared().isEnabled = true
+        // 获取用户授权通知（上方的提示条、声音、数字）
+        if #available(iOS 10.0, *) {
+            UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .carPlay, .sound]) { (success, error) in
+                print("授权 " + (success ? "成功" : "失败"))
+            }
+        } else {
+            let notiSettings = UIUserNotificationSettings(types: [.alert, .badge, .alert], categories: nil)
+            UIApplication.shared.registerUserNotificationSettings(notiSettings)
+        }
+    }
 }
 
