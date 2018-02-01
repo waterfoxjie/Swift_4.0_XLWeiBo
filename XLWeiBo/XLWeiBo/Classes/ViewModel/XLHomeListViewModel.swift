ff3c1576304521c3d6cb65e4bd2072ec2c09840f
waterfoxjie <waterfoxjie@163.com>
2018-02-01 17:42:38 +0800

    修改 XLHomeListViewModel 微博列表模型视图逻辑 1、将存储的数值类型修改为 XLHomeViewModel（单个微博视图模型） 2、字典转模型处做对应的修改

--- XLWeiBo/XLWeiBo/Classes/ViewModel/XLHomeListViewModel.swift
+++ XLWeiBo/XLWeiBo/Classes/ViewModel/XLHomeListViewModel.swift
@@ -9,9 +9,10 @@
 import Foundation
 import ObjectMapper
 
+/// 微博列表视图模型
 class XLHomeListViewModel {
-    // 微博模型数组懒加载
-    lazy var homeList = [XLHomeInfoModel]()
+    // 微博视图模型数组懒加载
+    lazy var homeList = [XLHomeViewModel]()
     // 是否需要请求网络
     private var isHasRequest = true
     
@@ -27,26 +28,37 @@ class XLHomeListViewModel {
             completion(true, false)
             return
         }
-        let since_id = isPullup ? 0 : homeList.first?.wbID ?? 0
-        let tempMaxId = isPullup ? homeList.last?.wbID ?? 0 : 0
+        let since_id = isPullup ? 0 : homeList.first?.homeInfo.wbID ?? 0
+        let tempMaxId = isPullup ? homeList.last?.homeInfo.wbID ?? 0 : 0
         let max_id = tempMaxId > 0 ? tempMaxId - 1 : tempMaxId
         // 调用网络方法加载数据
-        print("最后一条数据\(String(describing: homeList.last?.wbText))")
+        print("最后一条数据\(String(describing: homeList.last?.homeInfo.wbText))")
         XLNetworkManager.shareManager.homeTimelineRequest(since_id: Int64(since_id), max_id: Int64(max_id)) { (list, isSuccess) in
-            guard let array = Mapper<XLHomeInfoModel>().mapArray(JSONArray: list ?? []) as? [XLHomeInfoModel] else {
-                completion(isSuccess, false)
+            // 判断网络请求是否成功
+            if !isSuccess {
+                completion(false, false)
                 return
+            }
+            // 定义可变的视图模型数组
+            var viewModelArray = [XLHomeViewModel]()
+            // 遍历获取到的数据，进行字典转模型操作
+            for dict in list ?? [] {
+                // 若模型转换失败，则继续下一次操作
+                guard let model = Mapper<XLHomeInfoModel>().map(JSON: dict) else {
+                    continue
+                }
+                viewModelArray.append(XLHomeViewModel(model: model))
             }
             // 拼接数据
             if isPullup {
-                print("上拉刷新到\(array.count)数据")
-                self.homeList += array
+                print("上拉刷新到\(viewModelArray.count)数据")
+                self.homeList += viewModelArray
             } else {
-                print("下拉刷新到\(array.count)数据")
-                self.homeList = array + self.homeList
+                print("下拉刷新到\(viewModelArray.count)数据")
+                self.homeList = viewModelArray + self.homeList
             }
             // 判断上拉刷新数量
-            if isPullup && array.count == 0 {
+            if isPullup && viewModelArray.count == 0 {
                 self.isHasRequest = false
                 // 回调
                 completion(isSuccess, false)
