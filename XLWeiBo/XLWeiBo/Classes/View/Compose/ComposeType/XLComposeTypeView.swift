d8104d305cea4dfcd18c5be1035ef6546cb28ad4
waterfoxjie <waterfoxjie@163.com>
2018-03-06 13:26:25 +0800

    添加按钮并设置九宫格布局

--- XLWeiBo/XLWeiBo/Classes/View/Compose/ComposeType/XLComposeTypeView.swift
+++ XLWeiBo/XLWeiBo/Classes/View/Compose/ComposeType/XLComposeTypeView.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 
+private let ButtonMaxRow: CGFloat = 2
+private let ButtonMaxColum: CGFloat = 3
+
 class XLComposeTypeView: UIView {
 
     @IBOutlet weak var scrollView: UIScrollView!
@@ -50,8 +53,27 @@ class XLComposeTypeView: UIView {
 
 // 添加子控件
 private extension XLComposeTypeView {
-    func addComposeTypeBtn() {
-        
+    func setupUI() {
+        // 强制刷新（目的：获取控件的正确大小）
+        layoutIfNeeded()
+        // 获取 scrollView 的大小
+        let scrollViewRect = scrollView.bounds
+        // 一屏最多可显示数量
+        let showMaxCount: Int = Int(ButtonMaxRow * ButtonMaxColum)
+        // 一共需要分几屏
+        let viewCount = buttonInfo.count % showMaxCount > 0 ? buttonInfo.count / showMaxCount + 1 : buttonInfo.count / showMaxCount
+        // 添加对应的 View
+        for i in 0..<viewCount {
+            let v = UIView(frame: scrollViewRect.offsetBy(dx: CGFloat(i) * scrollViewRect.width, dy: 0))
+            addButtons(addView: v, fromIndex: i * showMaxCount, addCount: showMaxCount)
+            // 将视图添加到 scrollView 上
+            scrollView.addSubview(v)
+        }
+        // 设置 scrollView 属性
+        scrollView.contentSize = CGSize(width: CGFloat(viewCount) * scrollViewRect.width, height: 0)
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.bounces = false
     }
     
     /// 添加按钮
@@ -75,7 +97,21 @@ private extension XLComposeTypeView {
             }
             // 根据数据创建按钮
             let btn = XLComposeTypeButton.composeTypeButton(imageName: imageName, title: title)
+            // 设置 btn 的 frame
             addView.addSubview(btn)
+        }
+        // 遍历子视图，进行布局
+        for (idx, btn) in addView.subviews.enumerated() {
+            let btnSize = btn.bounds.size
+            // 行间距与列间距
+            let btnMargin = (addView.bounds.width - btnSize.width *  ButtonMaxColum) / (ButtonMaxColum + 1)
+            let btnPadding = addView.bounds.height - btnSize.height * ButtonMaxRow
+            // 获取当前对应的行、列
+            let row = idx % Int(ButtonMaxColum)
+            let colum = idx / Int(ButtonMaxColum)
+            let btnX = btnMargin + (btnSize.width + btnMargin) * CGFloat(row)
+            let btnY = (btnSize.height + btnPadding) * CGFloat(colum)
+            btn.frame = CGRect(origin: CGPoint(x: btnX, y: btnY), size: btnSize)
         }
     }
 }
