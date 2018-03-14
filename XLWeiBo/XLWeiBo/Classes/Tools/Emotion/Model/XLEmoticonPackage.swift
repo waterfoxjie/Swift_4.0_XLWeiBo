4169f8d3e642ef1a29fd7e3e78e368a7427c9e05
waterfoxjie <waterfoxjie@163.com>
2018-03-14 16:11:07 +0800

    设置表情模型数组的值

--- XLWeiBo/XLWeiBo/Classes/Tools/Emotion/Model/XLEmoticonPackage.swift
+++ XLWeiBo/XLWeiBo/Classes/Tools/Emotion/Model/XLEmoticonPackage.swift
@@ -13,7 +13,21 @@ class XLEmoticonPackage: Mappable {
     // 表情包名称
     var groupName: String?
     // 表情包目录，从目录下加载 info.plist 可以创建表情数组
-    var directory: String?
+    var directory: String? {
+        // 设置目录时，从目录中加载 info.plist
+        didSet {
+            guard let directory = directory,
+                let bundle = Bundle.main.emotionsBundle(),
+                // 拿到对应的 info.plist 文件目录
+                let plistPath = bundle.path(forResource: "info.plist", ofType: nil, inDirectory: directory),
+                let array = NSArray(contentsOfFile: plistPath) as? [[String: String]],
+                let modelArray = Mapper<XLEmoticonModel>().mapArray(JSONObject: array)
+            else {
+                return
+            }
+            emoticons += modelArray
+        }
+    }
     // 懒加载表情模型的空数组（使用懒加载可以避免后续解包）
     lazy var emoticons = [XLEmoticonModel]()
     
